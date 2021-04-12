ac19bc333758177179b41fdba99aaf0b53ee896e
esmoi <esmoi@student.matnat.uio.no>
2021-04-12 09:05:17 +0200

    trying to fix matplotlib issue

--- oblig2_mek11.py
+++ oblig2_mek11.py
@@ -1,8 +1,8 @@
 # bruker kode fra Appendiks A:
 import scipy.io as sio
-import matplotlib.pyplot as plt
 import numpy as np
-import numpy.matlib
+import matplotlib.pyplot as plt 
+
 # Dette virker med versjon 7 MAT-filer
 
 data = sio.loadmat('data.mat')
@@ -13,10 +13,16 @@ v = data.get('v')       # matrise V
 xit = data.get('xit')   # vektor XIT
 yit = data.get('yit')   # vektor YIT
 
-#print(y[100,0])
-print(u, len(v[:,0]))    
+velocity_array= np.sqrt(u**2+v**2) # beregner hastigheten
 
-# zero matrix with same shape as 
-# vel_field = np.matlib.zeros((194,201))
 
+# cmap = cm.winter is just a particular colour gradient 
+plt.contourf(x,y,velocity_array)
+
+# plotting interface between water and air
+# (35,160), (70, 170)
+#plt.quiver(x[34:69,159:169],y[34:69,159:169],velocity_array[34:69,159:169])
+#plt.quiver(x[34:69,84:99],y[34:69,84:99],velocity_array[34:69,84:99])
+#plt.quiver(x[34:69,49:59],y[34:69,49:59],velocity_array[34:69,49:59])
+plt.show()  
 
